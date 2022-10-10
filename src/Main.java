e57ec52428e9d06ae04dba28fd46876b8b1e2ab4
SaparDegen <saparbekdegenbaev@gmail.com>
2022-10-10 21:54:51 +0600

    Modified classes

--- src/Main.java
+++ src/Main.java
@@ -8,47 +8,72 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner scanner = new Scanner(System.in);
+    public static Scanner scannerS = new Scanner(System.in);
+    public static Scanner scannerN = new Scanner(System.in);
 
     public static void main(String[] args) {
         DeliveryCompanyService deliveryCompanyService = new DeliveryCompanyService();
+        options();
         while (true) {
-            options();
-            System.out.println("Choose the button:");
-            String word = scanner.nextLine();
+            System.out.print("Choose the button: ");
+            String word = scannerS.nextLine();
             switch (word) {
-                case "1" -> {
-                    deliveryCompanyService.createDeliveryCompany();
+                case "1": {
+                    System.out.print("Enter the name of Delivery company: ");
+                    String delivCompanyName = scannerS.nextLine();
+                    deliveryCompanyService.createDeliveryCompany(delivCompanyName);
+                    break;
                 }
-                case "2" -> {
-                    deliveryCompanyService.createCustomer();
+                case "2": {
+                    System.out.print("Enter the name of customer: ");
+                    String customerName = scannerS.nextLine();
+                    deliveryCompanyService.createCustomer(customerName);
+                    break;
                 }
-                case "3" -> {
-
-                    deliveryCompanyService.acceptOrder();
+                case "3": {
+                    System.out.print("Enter the name of Customer: ");
+                    String nameOfCustomer = scannerS.nextLine();
+                        System.out.print("Enter the height of load: ");
+                        double height = scannerN.nextDouble();
+                        System.out.print("Enter the width of load: ");
+                        double width = scannerN.nextDouble();
+                        System.out.print("Enter the length of load: ");
+                        double length = scannerN.nextDouble();
+                        System.out.print("Enter the weight of load in kg: ");
+                        double weight = scannerN.nextDouble();
+                    Load load = deliveryCompanyService.addLoad(height, width, length, weight);
+                    deliveryCompanyService.createOrder(nameOfCustomer, load);
+                    break;
                 }
-                case "4" -> {
-                    try {
-                        System.out.println("Enter the name of customer:");
-                        System.out.println(deliveryCompanyService.getTotalFee(scanner.nextLine()));
-                    } catch (NoSuchElementException e) {
-                        System.out.println("There is no customer with this name");
-                    }
+                case "4": {
+                    System.out.print("Enter the name of Customer: ");
+                    String nameOfCustomer = scannerS.nextLine();
+                    System.out.print("Enter the name of delivery company: ");
+                    String nameOfDelivCompany = scannerS.nextLine();
+                    deliveryCompanyService.acceptOrder(nameOfCustomer, nameOfDelivCompany);
+                    break;
                 }
-                case "5" -> {
-                    try {
-                        System.out.println("Enter the company's name:");
-                        System.out.println(deliveryCompanyService.getCompanyProfit(scanner.nextLine()));
-                    } catch (NoSuchElementException e) {
-                        System.out.println("No such company");
-                    }
-
+                case "5": {
+                    System.out.print("Enter the name of Customer: ");
+                    String nameOfCustomer = scannerS.nextLine();
+                    System.out.println("The total fee is: " + deliveryCompanyService.getTotalFee(nameOfCustomer));
+                    break;
                 }
-                case "6" -> {
-                    System.out.println(deliveryCompanyService.getDeliveryCompanies().toString());
+                case "6": {
+                    System.out.print("Enter the name of delivery company: ");
+                    String nameOfDelivCompany = scannerS.nextLine();
+                    System.out.println("The total profit of Delivery Company: " + deliveryCompanyService.getCompanyProfit(nameOfDelivCompany));
+                    break;
                 }
-                case "7" -> {
-                    System.out.println(deliveryCompanyService.getCustomersDataBase().toString());
+                case "7": {
+                    System.out.println(" CompanyName| PricePerKg | MaxPerCubSm| Customers              ");
+                    deliveryCompanyService.getDeliveryCompanies().stream().forEach(System.out::println);
+                    break;
+                }
+                case "8": {
+                    System.out.println(" CustomName | Orders              ");
+                    deliveryCompanyService.getCustomersDataBase().stream().forEach(System.out::println);
+                    break;
                 }
             }
             if (word.equals("x")) {
@@ -60,12 +85,13 @@ public class Main {
     static void options() {
         System.out.println("----------Commands------------");
         System.out.println("Press 1 to create Delivery Company");
-        System.out.println("Press 2 to create customer");
-        System.out.println("Press 3 to accept order");
-        System.out.println("Press 4 to get sum of total fees of exact customer");
-        System.out.println("Press 5 to get company's profit");
-        System.out.println("Press 6 to show all companies");
-        System.out.println("Press 7 to show all customers");
+        System.out.println("Press 2 to create Customer");
+        System.out.println("Press 3 to create order by Customer");
+        System.out.println("Press 4 to place an order to Delivery company");
+        System.out.println("Press 5 to get sum of total fees of exact customer");
+        System.out.println("Press 6 to get company's profit");
+        System.out.println("Press 7 to show all companies");
+        System.out.println("Press 8 to show all customers");
         System.out.println("Press x to finish program");
         System.out.println("-------------------------------");
     }
--- src/service/DeliveryCompanyService.java
+++ src/service/DeliveryCompanyService.java
@@ -13,113 +13,113 @@ import java.util.Scanner;
 public class DeliveryCompanyService {
     private List<DeliveryCompany> deliveryCompanies = new ArrayList<>();
     private List<Customer> customersDataBase = new ArrayList<>();
-    private List<Load> loadDataBase = new ArrayList<>();
 
-    public DeliveryCompanyService(List<DeliveryCompany> deliveryCompanies) {
-        this.deliveryCompanies = deliveryCompanies;
-    }
-
-    static Scanner scannerS = new Scanner(System.in);
-    static Scanner scannerN = new Scanner(System.in);
-
-    public DeliveryCompanyService() {
-
-    }
-
-    public void acceptOrder() {
-        System.out.println("Enter the name of delivery company:");
-        String nameOfDeliveryCompany = scannerS.nextLine();
-        DeliveryCompany deliveryCompany = getDeliveryCompanies().stream().filter(x -> x.getDeliveryCompanyName().equals(nameOfDeliveryCompany)).findFirst().get();
-        Order order = new Order(addLoad());
-        order.setOrderFee(deliveryCompany.getPricePerKilogram() * order.getLoad().getWeight());
-        double maxCapacity = deliveryCompany.getMaxCapacityPerCubeCentimeter();
-        System.out.println("Enter the name of customer:");
-        String nameOfCustomer = scannerS.nextLine();
-        if ((order.getLoad().getHeight()) * (order.getLoad().getWidth()) * (order.getLoad().getLength()) <= maxCapacity) {
-            Customer customer = getCustomersDataBase().stream().filter(x -> x.getCustomerName().equals(nameOfCustomer)).findFirst().get();
-            customer.addOrder(order);
-            deliveryCompany.getCustomers().add(customer);
-        } else {
-            System.out.println("Sorry, but your load is too big");
-        }
-    }
-
-    public Load addLoad() {
-        System.out.println("Enter the height of load");
-        double height = scannerN.nextDouble();
-        System.out.println("Enter the width of load");
-        double width = scannerN.nextDouble();
-        System.out.println("Enter the length of load");
-        double length = scannerN.nextDouble();
-        System.out.println("Enter the weight of load in kg");
-        double weight = scannerN.nextDouble();
-        getLoadDataBase().add(new Load(height, width, length, weight));
-        return getLoadDataBase().stream().filter(x -> x.getHeight() == height && x.getWidth() == width && x.getLength() == length && x.getWeight() == weight)
-                .findFirst().get();
-    }
-    public void createDeliveryCompany() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the name of company: ");
-        String name = scanner.nextLine();
-        getDeliveryCompanies().add(new DeliveryCompany(name));
-    }
-
-    public void createCustomer() {
-        System.out.println("Enter the name of customer:");
-        String name = scannerS.nextLine();
-        getCustomersDataBase().add(new Customer(name));
-    }
-
-    public double getTotalFee(String nameOfCustomer) {
-        Customer customer = getCustomersDataBase().stream().filter(x -> x.getCustomerName().equals(nameOfCustomer)).findFirst().get();
-        double sum = customer.getOrders().stream().mapToDouble(x -> x.getOrderFee()).reduce(0.0, Double::sum);
-        return sum;
-    }
-
-    public double getCompanyProfit(String deliveryCompanyName) {
-        DeliveryCompany deliveryCompany = getDeliveryCompanies().stream().filter(x -> x.getDeliveryCompanyName().equals(deliveryCompanyName)).findFirst().get();
-        double numberOfCustomerWhichPaid = deliveryCompany.getCustomers().stream().map(x -> x.getOrders().stream().mapToDouble(a -> a.getOrderFee()).reduce(0.0, Double::sum)).count();
-        int sum = 0;
-        for (int i = 0; i < numberOfCustomerWhichPaid; i++) {
-            sum += deliveryCompany.getCustomers().get(i).getOrders().stream().mapToDouble(x -> x.getOrderFee()).reduce(0.0, Double::sum);
-        }
-        return sum;
-    }
+    public DeliveryCompanyService() {}
 
     public List<DeliveryCompany> getDeliveryCompanies() {
         return deliveryCompanies;
-    }
-
-    public void setDeliveryCompanies(List<DeliveryCompany> deliveryCompanies) {
-        this.deliveryCompanies = deliveryCompanies;
     }
 
     public List<Customer> getCustomersDataBase() {
         return customersDataBase;
     }
 
-    public void setCustomers(List<Customer> customersDataBase) {
-        this.customersDataBase = customersDataBase;
+    public void createDeliveryCompany(String delivCompanyName) {
+        DeliveryCompany deliveryCompany = new DeliveryCompany(delivCompanyName);
+        deliveryCompanies.add(deliveryCompany);
+        System.out.println("A new Delivery company " + delivCompanyName + " was created");
     }
 
-    public void setCustomersDataBase(List<Customer> customersDataBase) {
-        this.customersDataBase = customersDataBase;
+    public void createCustomer(String customerName) {
+        Customer customer = new Customer(customerName);
+        customersDataBase.add(customer);
+        System.out.println("A new Customer " + customerName + " was created");
     }
 
-    public List<Load> getLoadDataBase() {
-        return loadDataBase;
+    public Load addLoad(double height, double width, double length, double weight) {
+        Load load = new Load(height, width, length, weight);
+        return load;
     }
 
-    public void setLoadDataBase(List<Load> loadDataBase) {
-        this.loadDataBase = loadDataBase;
+    public void createOrder(String customerName, Load load) {
+        Order order = new Order(load);
+        Customer customer = getCustomersDataBase().stream().filter(x -> x.getCustomerName().equals(customerName)).findAny().orElse(null);
+        if (customer == null) {
+            try {
+                throw new Exception("The entered client was not found.");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            customer.setOrder(order);
+        }
     }
 
-    @Override
-    public String toString() {
-        return "DeliveryCompanyService{" +
-                "deliveryCompanies=" + deliveryCompanies +
-                ", customersDataBase=" + customersDataBase +
-                ", loadDataBase=" + loadDataBase +
-                '}';
+    public void acceptOrder(String customerName, String delivCompanyName) {
+        Customer customer = getCustomersDataBase().stream().filter(x -> x.getCustomerName().equals(customerName)).findAny().orElse(null);
+        if (customer == null) {
+            try {
+                throw new Exception("The entered client was not found.");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            DeliveryCompany delivComp = getDeliveryCompanies().stream().filter(x -> x.getDeliveryCompanyName().equals(delivCompanyName)).findAny().orElse(null);
+            if (delivComp == null) {
+                try {
+                    throw new Exception("The entered Delivery company was not found.");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            } else {
+                if (customer.getOrder() != null) {
+                    Order order = customer.getOrder();
+                    Load load = order.getLoad();
+                    double maxCapacity = delivComp.getMaxCapacityPerCubeCentimeter();
+                    if ((load.getHeight() * load.getWidth() * load.getLength()) <= maxCapacity) {
+                        order.setOrderFee(delivComp.getPricePerKilogram() * load.getWeight());
+                        delivComp.getCustomers().add(customer);
+                    } else {
+                        System.out.println("Sorry, but your load is too big");
+                    }
+                } else {
+                    System.out.println("Sorry, but firstly create an order by Customer");
+                }
+            }
+        }
+    }
+
+    public double getTotalFee(String nameOfCustomer) {
+        double totalFee = 0;
+        Customer customer = getCustomersDataBase().stream().filter(x -> x.getCustomerName().equals(nameOfCustomer)).findAny().orElse(null);
+        if (customer == null) {
+            try {
+                throw new Exception("The entered client was not found.");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            if (customer.getOrder() != null) {
+                totalFee = customer.getOrder().getOrderFee();
+            }
+        }
+        return totalFee;
+    }
+
+    public double getCompanyProfit(String delivCompName) {
+        double profitAmount = 0;
+        DeliveryCompany deliveryCompany = getDeliveryCompanies().stream().filter(x -> x.getDeliveryCompanyName().equals(delivCompName)).findAny().orElse(null);
+        if (deliveryCompany == null) {
+            try {
+                throw new Exception("The entered Delivery company was not found.");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            if (deliveryCompany.getCustomers() != null) {
+                profitAmount = deliveryCompany.getCustomers().stream().map(x -> x.getOrder().getOrderFee()).reduce((double) 0, (a1, a2) -> a1 + a2);
+            }
+        }
+        return profitAmount;
     }
 }
